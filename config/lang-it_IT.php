d18975ff30a27ffd251e24a1aecb5e063736984f
Riccardo <r.bicelli@gmail.com>
2019-10-17 18:11:09 +0200

    Added first language

---
+++ config/lang-it_IT.php
@@ -0,0 +1,16 @@
+<?php
+
+$lang['Mail Release'] =	'Rilascio Messaggi Mail';
+$lang['Now you can close the browser window'] =	'Ora puoi chiudere la finetra del browser.';
+$lang['Release result'] = 'Risultato del Rilascio';
+$lang['Close'] = 'Chiudi';
+$lang['Warning'] = 'Attenzione';
+$lang['Sure to release message'] = 'Vuoi rilasciare il messaggio con ID di riferimento <b>{1}</b> dalla quarantena?';
+$lang['Release recommendation'] = 'Rilascia il messaggio solo se sei assolutamente sicuro che si tratti di una comunicazione $
+$lang['Release alert'] = 'Stai agendo sotto la tua responsabilità! <br> Se la posta contiene davvero un virus, può causare da$
+$lang['Release Mail'] =	'Rilascia Messaggio';
+$lang['Release Succeeded'] = "<b>Rilascio riuscito!</b> controlla la tua casella di posta";
+$lang['Release Failed']	= "<b>Errore!</b> Impossibile eseguire il rilascio. Contatta il tuo amministratore di sistema."; 
+$lang['Close'] = 'Chiudi';
+
+?>
