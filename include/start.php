ef5682d6f561ad511a00a8d1232f5122889590a6
Peter Bieringer <pb@bieringer.de>
2022-04-10 10:07:57 +0200

    add backwards compatibility for config.php

--- include/start.php
+++ include/start.php
@@ -2,4 +2,8 @@
   include('config.php');
   include('lang-' . $language . '.php');
   include('functions.php');
+
+  // fallback for config.php < 2022-04-10 (typo fixed)
+  if (isset($recaptchca_api_key   ) && ! isset($recaptcha_api_key   )) { $recaptcha_api_key    = $recaptchca_api_key;    };
+  if (isset($recaptchca_secret_key) && ! isset($recaptcha_secret_key)) { $recaptcha_secret_key = $recaptchca_secret_key; };
 ?>
